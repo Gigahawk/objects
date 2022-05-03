ef836579539bf01145b8aadfb978ff1caf9a62de
Jasper Chan <jasperchan515@gmail.com>
2022-05-02 23:23:41 -0700

    paper_tray_pillar: shrink dimensions, add chamfers

--- paper_tray_pillar.py
+++ paper_tray_pillar.py
@@ -2,13 +2,15 @@
 
 import cadquery as cq
 
-width = 17.2
+width = 17.1
 
-insert_thickness = 4
+insert_thickness = 3.8
 insert_depth = 12
 
 pillar_thickness = 8
 pillar_height = 75
+
+chamfer = 2
 
 result = (
     cq.Workplane("XY").tag("base_plane")
@@ -17,4 +19,5 @@ result = (
     .rect(width, pillar_thickness, centered=[True, False]).extrude(pillar_height)
     .faces(">Z").workplane()
     .rect(width, insert_thickness, centered=[True, False]).extrude(insert_depth)
+    .edges("|Y").chamfer(chamfer)
 )
