f553b95920e84f363059d72e191e896ba25ffe83
Jasper Chan <jasperchan515@gmail.com>
2022-06-07 02:04:06 -0700

    desktop_battery_tray: center pockets

--- desktop_battery_tray.py
+++ desktop_battery_tray.py
@@ -7,26 +7,32 @@ height = 60
 thickness = 15
 chamfer = 3
 
-offset_9v = 5.0
 depth_9v = 17.0
 width_9v = 26.5
 height_9v = 48.5
 
-offset_aa = 40
 diameter_aa = 14.5
 length_aa = 50.5
 
-result = (
-    cq.Workplane("XY").tag("base_plane")
-    # Base geometry
-    .rect(width, height, centered=[True, False]).extrude(thickness)
-    # External chamfers
-    .edges("|Z").chamfer(chamfer)
-    .faces(">Z").chamfer(chamfer)
-    # 9V pocket
-    .faces(">Z").workplane().center(0, offset_9v)
-    .rect(height_9v, width_9v, centered=[True, False]).cutBlind(-depth_9v/2)
-    # AA pocket
-    .faces(">X").workplane().workplane(offset=-(width + length_aa)/2).center(offset_aa, 0)
-    .circle(radius=diameter_aa/2).cutBlind(length_aa)
-)
+pocket_gap = 2
+pocket_edge_to_edge = width_9v + pocket_gap + diameter_aa
+offset_9v = (height - pocket_edge_to_edge)/2
+# For some reason AA cut plane origin is at offset_9v already
+offset_aa = width_9v + pocket_gap + diameter_aa/2
+
+result = cq.Workplane("XY").tag("base_plane")
+
+# Base geometry
+result = result.rect(width, height, centered=[True, False]).extrude(thickness)
+
+# External chamfers
+result = result.edges("|Z").chamfer(chamfer)
+result = result.faces(">Z").chamfer(chamfer)
+
+# 9V pocket
+result = result.faces(">Z").workplane().center(0, offset_9v)
+result = result.rect(height_9v, width_9v, centered=[True, False]).cutBlind(-depth_9v/2)
+
+# AA pocket
+result = result.faces(">X").workplane(offset=-(width + length_aa)/2).center(offset_aa, 0)
+result = result.circle(radius=diameter_aa/2).cutBlind(length_aa)
