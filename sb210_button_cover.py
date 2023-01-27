1b92d2836429dda2dc310351a478634c57a398ed
Jasper Chan <jasperchan515@gmail.com>
2023-01-27 01:54:12 -0800

    sb210_button_cover: add chamfer to button openings

--- sb210_button_cover.py
+++ sb210_button_cover.py
@@ -9,6 +9,8 @@ bulk_thickness = 3
 button_depth = 2.6
 button_width = 6.2
 button_distance = 13.75
+button_chamfer1 = 0.2
+button_chamfer2 = 1.0
 
 fillet = 2
 
@@ -21,4 +23,7 @@ result = (
     .rarray(button_distance, 0.1, 3, 1, center=True)
     .rect(button_width, button_width)
     .cutBlind(-button_depth)
+    # Not sure why you can't do `and not()`
+    .edges(">Z and #Z").edges("not(>Y or >X or <Y or <X)")
+    .chamfer(button_chamfer1, button_chamfer2)
 )
