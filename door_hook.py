6c11d47a47379d86282fcdb17db7a8b0e00f18a8
Jasper Chan <jasperchan515@gmail.com>
2023-09-05 19:58:35 -0700

    door_hook: add model for a coat hook that hangs off a door

---
+++ door_hook.py
@@ -0,0 +1,30 @@
+"""Hook that goes over the edge of a door"""
+
+from build123d import *
+
+# Thickness of the door
+door_thickness = 20
+# Thickness of the part
+thickness = 5
+# Length of the part that hangs behind the door
+support_length = 10
+width = 30
+length = 70
+hook_dia = 40
+
+with BuildPart() as part:
+    with BuildSketch() as sketch:
+        with BuildLine() as path:
+            l1 = Line((0, 0), (0, support_length))
+            l2 = Line(l1@1, l1@1 + (door_thickness, 0))
+            l3 = Line(l2@1, l2@1 - (0, length))
+            l4 = TangentArc(l3@1, l3@1 + (hook_dia + thickness, 0), tangent=l3 % 1)
+            offset(amount=thickness/4, side=Side.LEFT, closed=False, kind=Kind.TANGENT)
+            offset(amount=thickness/4)
+        make_face()
+    extrude(amount=width)
+
+result = part.part
+
+if "show_object" in locals():
+    show_object(part)
