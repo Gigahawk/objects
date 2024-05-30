8c2cca1ee4e2a3106e2cddf8b37b9fb768f9b018
Jasper Chan <jasperchan515@gmail.com>
2024-05-29 23:18:09 -0700

    pcie_bracket_ssd1306_128x32_mount: improve removability of supports, add right side clip
    * tweaked tolerances

--- pcie_bracket_ssd1306_128x32_mount.py
+++ pcie_bracket_ssd1306_128x32_mount.py
@@ -12,8 +12,10 @@ pcb_width = 38.2
 pcb_height = 12.1
 pcb_vertical_offset = 0
 module_thickness = 3.2
-tolerance = 0.5
-guide_thickness = module_thickness - tolerance
+tolerance = 0.25
+guide_thickness = module_thickness
+guide_support_hole_width = 7
+guide_support_chamfer_depth = 2
 tab_extra_support_width = 10
 tab_gap = 0.2
 tab_thickness = 1.2
@@ -25,10 +27,11 @@ cutout_taper = 45
 cutout_horizontal_offset = 0
 cutout_vertical_offset = 0
 window_support_gap = 2
-clip_offset = 5
+clip_offset = 3
 clip_width = 10
 clip_thickness = 2
 clip_overhang = 0.6
+clip_chamfer_angle = 30
 
 _cutout_vertical_offset = cutout_vertical_offset + pcb_vertical_offset
 _pcb_width = pcb_width + tolerance
@@ -57,13 +60,13 @@ with BuildPart() as mount:
                     tab_extra_support_width, main_height,
                     align=(Align.MIN, Align.MAX)
                 )
-        with Locations((0, pcb_vertical_offset)):
+        with Locations((0, mount_face.center().Z - main_height/2 + pcb_vertical_offset)):
             Rectangle(
                 _pcb_width, _pcb_height,
                 mode=Mode.SUBTRACT)
     extrude(amount=guide_thickness)
     guide_face = mount.faces().sort_by(Axis.Y)[0]
-    guide_right_face = mount.faces(Select.LAST).filter_by(Axis.X).sort_by(Axis.X, reverse=True)[1]
+    guide_right_face = mount.faces(Select.LAST).filter_by(Axis.X).sort_by(Axis.X, reverse=True)[0]
     tab_edge = guide_face.edges().filter_by(Axis.Z).sort_by(Axis.X)[0]
     with BuildSketch() as tab_sketch:
         with Locations(tab_edge.start_point()):
@@ -99,14 +102,14 @@ with BuildPart() as mount:
     chamfer(tab_support_edge, guide_thickness + tab_gap + tab_thickness - 0.0001)
 
     with BuildSketch(mount_face) as cutout_sketch:
-        with Locations((cutout_horizontal_offset, -_cutout_vertical_offset)):
+        with Locations((cutout_horizontal_offset, mount_face.center().Z - main_height/2 -_cutout_vertical_offset)):
             Rectangle(cutout_width, cutout_height)
     extrude(
         amount=-thickness, mode=Mode.SUBTRACT,
         taper=-cutout_taper
     )
     with BuildSketch(mount_face) as cutout_support_sketch:
-        with Locations((cutout_horizontal_offset, -_cutout_vertical_offset)):
+        with Locations((cutout_horizontal_offset, mount_face.center().Z - main_height/2 -_cutout_vertical_offset)):
             Rectangle(
                 cutout_width - window_support_gap*2,
                 cutout_height - support_gap*2,
@@ -115,18 +118,55 @@ with BuildPart() as mount:
         amount=-thickness, taper=-cutout_taper
     )
     with BuildSketch(mount_face.offset(support_gap)) as guide_support_sketch:
-        with Locations((0, pcb_vertical_offset)):
+        with Locations((0, mount_face.center().Z - main_height/2 + pcb_vertical_offset)):
             Rectangle(
                 _pcb_width - 2*window_support_gap,
-                _pcb_height - 2*support_gap)
+                _pcb_height - 2*support_gap
+            )
+            Rectangle(
+                guide_support_hole_width, guide_support_hole_width,
+                mode=Mode.SUBTRACT
+            )
     extrude(
         amount=guide_thickness - support_gap - tab_gap
     )
-    with BuildSketch(guide_right_face.offset(clip_offset)) as clip_sketch:
-        with Locations((0, guide_thickness/2)):
-            Rectangle(main_height, clip_thickness, align=(Align.CENTER, Align.MIN))
-            #with BuildLine() as profile:
-            #    Line((pcb_height + tolerance/2 - clip_overhang, 0))
+    guide_support_chamfer_edge = (
+        mount.edges(Select.LAST).filter_by(Axis.Z)
+        .sort_by(Axis.X, reverse=True)[0:2].sort_by(Axis.Y, reverse=True)[0]
+    )
+    chamfer(
+        guide_support_chamfer_edge,
+        length=guide_thickness - support_gap - tab_gap - 0.0001,
+        length2=guide_support_chamfer_depth,
+    )
+    with BuildSketch(guide_right_face.offset(-guide_thickness - clip_offset)) as clip_sketch:
+        with Locations((0, -guide_thickness/2)):
+            Rectangle(main_height, clip_thickness, align=(Align.CENTER, Align.MAX))
+        # BuildLine doesn't inherit Locations
+        with BuildLine() as path:
+            PolarLine(
+                (
+                    _pcb_height/2 - clip_overhang,
+                    -guide_thickness/2
+                ),
+                angle=-90 + clip_chamfer_angle,
+                length=clip_thickness*3
+            )
+        with BuildLine() as profile:
+            PolarLine(
+                (
+                    _pcb_height/2 - clip_overhang,
+                    -guide_thickness/2
+                ),
+                angle=180,
+                length=support_gap
+            )
+        mirror(
+            sweep(profile.line, path.line, mode=Mode.SUBTRACT),
+            about=Plane(Location((0, 0, 0), (0, -90, 0))),
+            mode=Mode.SUBTRACT
+        )
+    extrude(amount=-clip_width)
 
 result = mount.part
 
