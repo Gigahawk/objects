bd50deea7e2be300621d5821a4826b3e457666cc
Jasper Chan <jasperchan515@gmail.com>
2024-10-21 22:24:02 +0000

    feature: export script supports specifying which parts to export

--- export.py
+++ export.py
@@ -1,3 +1,4 @@
+import click
 from pathlib import Path
 import os
 import shutil
@@ -134,49 +135,53 @@ def export_cadquery(result, path):
 
 
 
-def main():
+@click.command()
+@click.option("-f", "--file", "files", multiple=True)
+def main(files):
     print("Cleaning export folder")
+    if files:
+        files = [Path(f) for f in files]
+    else:
+        files = Path(".").glob("**/*.py")
     try:
         shutil.rmtree("export")
     except FileNotFoundError:
         pass
     os.makedirs("export")
-    for root, _, files in os.walk("."):
-        for f in files:
-            path = Path(root) / f
-            if path.suffix == ".py" and path.stem != "export":
-                module = ".".join(path.with_suffix("").parts)
-                print(f"Importing {module}")
-                mod = import_module(module)
-                if hasattr(mod, 'result'):
-                    results = {"default": mod.result}
-                elif hasattr(mod, 'results'):
-                    results = mod.results
-                else:
-                    print(
-                        f"Warning: Module {module} doesn't contain any results")
-                    continue
+    for path in files:
+        if path.suffix == ".py" and path.stem != "export":
+            module = ".".join(path.with_suffix("").parts)
+            print(f"Importing {module}")
+            mod = import_module(module)
+            if hasattr(mod, 'result'):
+                results = {"default": mod.result}
+            elif hasattr(mod, 'results'):
+                results = mod.results
+            else:
+                print(
+                    f"Warning: Module {module} doesn't contain any results")
+                continue
 
-                for name, result in results.items():
-                    for t in export_types:
-                        export_path = (
-                            Path("export") / (
-                                path
-                                .with_stem(path.stem + f"-{name}")
-                                .with_suffix(t)
-                            )
+            for name, result in results.items():
+                for t in export_types:
+                    export_path = (
+                        Path("export") / (
+                            path
+                            .with_stem(path.stem + f"-{name}")
+                            .with_suffix(t)
                         )
-                        if isinstance(result, cq.Assembly):
-                            result = result.toCompound()
-                        # Ensure export dir exists prior to writing to it
-                        export_dir = export_path.parent
-                        os.makedirs(export_dir, exist_ok=True)
-                        if "cadquery" in str(type(result)):
-                            print(f"Exporting CadQuery part to {export_path}")
-                            export_cadquery(result, export_path)
-                        else:
-                            print(f"Exporting build123d part to {export_path}")
-                            export_build123d(result, export_path)
+                    )
+                    if isinstance(result, cq.Assembly):
+                        result = result.toCompound()
+                    # Ensure export dir exists prior to writing to it
+                    export_dir = export_path.parent
+                    os.makedirs(export_dir, exist_ok=True)
+                    if "cadquery" in str(type(result)):
+                        print(f"Exporting CadQuery part to {export_path}")
+                        export_cadquery(result, export_path)
+                    else:
+                        print(f"Exporting build123d part to {export_path}")
+                        export_build123d(result, export_path)
 
 if __name__ == "__main__":
     main()
