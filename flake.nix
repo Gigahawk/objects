184f4e42717166ee1f7ebf5d96f0eecc98946ada
Jasper Chan <jasperchan515@gmail.com>
2024-10-21 21:35:40 +0000

    env: update flake

--- flake.nix
+++ flake.nix
@@ -3,48 +3,50 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    # https://github.com/NixOS/nixpkgs/pull/326696
-    nixpkgs-casadi.url = "github:Sigmanificient/nixpkgs?ref=AeroSandbox";
     flake-utils.url = "github:numtide/flake-utils";
     poetry2nix.url = "github:nix-community/poetry2nix";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-casadi, flake-utils, poetry2nix, ... }:
+  outputs = { self, nixpkgs, flake-utils, poetry2nix, ... }:
     flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs { inherit system; };
-      pkgs-casadi = import nixpkgs-casadi { inherit system; };
       inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryEnv defaultPoetryOverrides;
       poetryEnv = mkPoetryEnv {
         projectDir = ./.;
+        python = pkgs.python311;
         # https://github.com/nix-community/poetry2nix/blob/master/docs/edgecases.md
         overrides = defaultPoetryOverrides.extend
           (final: prev: {
             cq-warehouse = prev.cq-warehouse.overridePythonAttrs
               ( old: { buildInputs = (old.buildInputs or [ ]) ++ [ prev.setuptools ]; } );
-            trianglesolver = prev.trianglesolver.overridePythonAttrs
-              ( old: { buildInputs = (old.buildInputs or [ ]) ++ [ prev.setuptools ]; } );
-            numpy-quaternion = prev.numpy-quaternion.overridePythonAttrs
-              ( old: { buildInputs = (old.buildInputs or [ ]) ++ [ prev.setuptools ]; } );
-            # Not sure why just overriding build inputs doesn't work for this package
-            casadi = pkgs-casadi.python312Packages.casadi;
-            #casadi = prev.casadi.override {
-            #  preferWheel = true;
-            #  buildInputs = [ ];
-            #};
-            #casadi = prev.casadi.overridePythonAttrs
-            #  ( old: {
-            #    buildInputs = (old.buildInputs or [ ]) ++ [ prev.setuptools ];
-            #    nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ prev.cmake ];
-            #  });
             ocp-tessellate = prev.ocp-tessellate.overridePythonAttrs
-              ( old: { buildInputs = (old.buildInputs or [ ]) ++ [ prev.setuptools ]; } );
-            cadquery = prev.cadquery.overridePythonAttrs
               ( old: { buildInputs = (old.buildInputs or [ ]) ++ [ prev.setuptools ]; } );
             svgpathtools = prev.svgpathtools.overridePythonAttrs
               ( old: { buildInputs = (old.buildInputs or [ ]) ++ [ prev.setuptools ]; } );
             ocpsvg = prev.ocpsvg.overridePythonAttrs
               ( old: { buildInputs = (old.buildInputs or [ ]) ++ [ prev.setuptools ]; } );
+            trianglesolver = prev.trianglesolver.overridePythonAttrs
+              ( old: { buildInputs = (old.buildInputs or [ ]) ++ [ prev.setuptools ]; } );
+            ## Not sure why just overriding build inputs doesn't work for this package
+            casadi = pkgs.python311Packages.casadi;
+            #casadi = prev.casadi.override {
+            #  preferWheel = true;
+            #  buildInputs = [ ];
+            #};
+            ##casadi = prev.casadi.overridePythonAttrs
+            ##  ( old: {
+            ##    buildInputs = (old.buildInputs or [ ]) ++ [ prev.setuptools ];
+            ##    nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ prev.cmake ];
+            ##  });
+            cadquery = prev.cadquery.overridePythonAttrs
+              ( old: { buildInputs = (old.buildInputs or [ ]) ++ [ prev.setuptools ]; } );
+            ipykernel = pkgs.python311Packages.ipykernel;
+            ocp-vscode = prev.ocp-vscode.overridePythonAttrs
+              ( old: { buildInputs = (old.buildInputs or [ ]) ++ [ prev.setuptools ]; } );
+            ipython = pkgs.python311Packages.ipython;
+            typing-extensions = pkgs.python311Packages.typing-extensions;
+            six = pkgs.python311Packages.six;
           });
       };
     in {
