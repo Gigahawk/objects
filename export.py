88063ed17e2a13018875a0bd6c0c6d33f8b93e47
Jasper Chan <jasperchan515@gmail.com>
2024-11-02 22:07:52 -0700

    bugfix: ignore git folder in export

--- export.py
+++ export.py
@@ -149,39 +149,44 @@ def main(files):
         pass
     os.makedirs("export")
     for path in files:
-        if path.suffix == ".py" and path.stem != "export":
-            module = ".".join(path.with_suffix("").parts)
-            print(f"Importing {module}")
-            mod = import_module(module)
-            if hasattr(mod, 'result'):
-                results = {"default": mod.result}
-            elif hasattr(mod, 'results'):
-                results = mod.results
-            else:
-                print(
-                    f"Warning: Module {module} doesn't contain any results")
-                continue
+        if (
+            path.suffix != ".py"
+            or path.stem == "export"
+            or str(path).startswith(".")
+        ):
+            continue
+        module = ".".join(path.with_suffix("").parts)
+        print(f"Importing {module}")
+        mod = import_module(module)
+        if hasattr(mod, 'result'):
+            results = {"default": mod.result}
+        elif hasattr(mod, 'results'):
+            results = mod.results
+        else:
+            print(
+                f"Warning: Module {module} doesn't contain any results")
+            continue
 
-            for name, result in results.items():
-                for t in export_types:
-                    export_path = (
-                        Path("export") / (
-                            path
-                            .with_stem(path.stem + f"-{name}")
-                            .with_suffix(t)
-                        )
+        for name, result in results.items():
+            for t in export_types:
+                export_path = (
+                    Path("export") / (
+                        path
+                        .with_stem(path.stem + f"-{name}")
+                        .with_suffix(t)
                     )
-                    if isinstance(result, cq.Assembly):
-                        result = result.toCompound()
-                    # Ensure export dir exists prior to writing to it
-                    export_dir = export_path.parent
-                    os.makedirs(export_dir, exist_ok=True)
-                    if "cadquery" in str(type(result)):
-                        print(f"Exporting CadQuery part to {export_path}")
-                        export_cadquery(result, export_path)
-                    else:
-                        print(f"Exporting build123d part to {export_path}")
-                        export_build123d(result, export_path)
+                )
+                if isinstance(result, cq.Assembly):
+                    result = result.toCompound()
+                # Ensure export dir exists prior to writing to it
+                export_dir = export_path.parent
+                os.makedirs(export_dir, exist_ok=True)
+                if "cadquery" in str(type(result)):
+                    print(f"Exporting CadQuery part to {export_path}")
+                    export_cadquery(result, export_path)
+                else:
+                    print(f"Exporting build123d part to {export_path}")
+                    export_build123d(result, export_path)
 
 if __name__ == "__main__":
     main()
