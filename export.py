bfc0f1da8ff87fb9defd72b83bf1f3e21b33d20a
Jasper Chan <jasperchan515@gmail.com>
2025-09-29 11:13:12 -0700

    env: update b3d to 0.9.1 and other deps

--- export.py
+++ export.py
@@ -7,7 +7,7 @@ from importlib import import_module
 
 import cadquery as cq
 from cadquery import exporters
-from build123d import Mesher
+from build123d import Mesher, export_step, export_brep, ShapeList, Compound
 
 
 export_types = [
@@ -25,108 +25,115 @@ from typing import Iterable, Union
 from build123d.build_enums import MeshType
 from build123d.topology import Shape, Compound
 from py_lib3mf import Lib3MF
+
+
 def add_shape(
-        self,
-        shape: Union[Shape, Iterable[Shape]],
-        linear_deflection: float = 0.001,
-        angular_deflection: float = 0.1,
-        mesh_type: MeshType = MeshType.MODEL,
-        part_number: str = None,
-        uuid_value: uuid = None,
-    ):
-        """add_shape
+    self,
+    shape: Union[Shape, Iterable[Shape]],
+    linear_deflection: float = 0.001,
+    angular_deflection: float = 0.1,
+    mesh_type: MeshType = MeshType.MODEL,
+    part_number: str = None,
+    uuid_value: uuid = None,
+):
+    """add_shape
 
-        Add a shape to the 3MF/STL file.
+    Add a shape to the 3MF/STL file.
 
-        Args:
-            shape (Union[Shape, Iterable[Shape]]): build123d object
-            linear_deflection (float, optional): mesh control for edges. Defaults to 0.001.
-            angular_deflection (float, optional): mesh control for non-planar surfaces.
-                Defaults to 0.1.
-            mesh_type (MeshType, optional): 3D printing use of mesh. Defaults to MeshType.MODEL.
-            part_number (str, optional): part #. Defaults to None.
-            uuid_value (uuid, optional): value from uuid package. Defaults to None.
+    Args:
+        shape (Union[Shape, Iterable[Shape]]): build123d object
+        linear_deflection (float, optional): mesh control for edges. Defaults to 0.001.
+        angular_deflection (float, optional): mesh control for non-planar surfaces.
+            Defaults to 0.1.
+        mesh_type (MeshType, optional): 3D printing use of mesh. Defaults to MeshType.MODEL.
+        part_number (str, optional): part #. Defaults to None.
+        uuid_value (uuid, optional): value from uuid package. Defaults to None.
 
-        Rasises:
-            Warning: 3mf mesh is invalid
-            Warning: Degenerate shape skipped
-            Warning: 3mf mesh is not manifold
-        """
-        shapes = []
-        for input_shape in shape if isinstance(shape, Iterable) else [shape]:
-            if isinstance(input_shape, Compound):
-                shapes.extend(list(input_shape))
-            else:
-                shapes.append(input_shape)
+    Rasises:
+        Warning: 3mf mesh is invalid
+        Warning: Degenerate shape skipped
+        Warning: 3mf mesh is not manifold
+    """
+    shapes = []
+    for input_shape in shape if isinstance(shape, Iterable) else [shape]:
+        if isinstance(input_shape, Compound):
+            shapes.extend(list(input_shape))
+        else:
+            shapes.append(input_shape)
 
-        for b3d_shape in shapes:
-            # Create a 3MF mesh object
-            mesh_3mf: Lib3MF.MeshObject = self.model.AddMeshObject()
+    for b3d_shape in shapes:
+        # Create a 3MF mesh object
+        mesh_3mf: Lib3MF.MeshObject = self.model.AddMeshObject()
 
-            # Mesh the shape
-            ocp_mesh_vertices, triangles = Mesher._mesh_shape(
-                copy.deepcopy(b3d_shape),
-                linear_deflection,
-                angular_deflection,
-            )
+        # Mesh the shape
+        ocp_mesh_vertices, triangles = Mesher._mesh_shape(
+            copy.deepcopy(b3d_shape),
+            linear_deflection,
+            angular_deflection,
+        )
 
-            # Skip invalid meshes
-            if len(ocp_mesh_vertices) < 3 or not triangles:
-                warnings.warn(f"Degenerate shape {b3d_shape} - skipped")
-                continue
+        # Skip invalid meshes
+        if len(ocp_mesh_vertices) < 3 or not triangles:
+            warnings.warn(f"Degenerate shape {b3d_shape} - skipped")
+            continue
 
-            # Create 3mf mesh inputs
-            vertices_3mf, triangles_3mf = Mesher._create_3mf_mesh(
-                ocp_mesh_vertices, triangles
-            )
+        # Create 3mf mesh inputs
+        vertices_3mf, triangles_3mf = Mesher._create_3mf_mesh(
+            ocp_mesh_vertices, triangles
+        )
 
-            # Build the mesh
-            mesh_3mf.SetGeometry(vertices_3mf, triangles_3mf)
+        # Build the mesh
+        mesh_3mf.SetGeometry(vertices_3mf, triangles_3mf)
 
-            # Add the mesh properties
-            mesh_3mf.SetType(Mesher._map_b3d_mesh_type_3mf[mesh_type])
-            if b3d_shape.label:
-                mesh_3mf.SetName(b3d_shape.label)
-            if part_number:
-                mesh_3mf.SetPartNumber(part_number)
-            if uuid_value:
-                mesh_3mf.SetUUID(str(uuid_value))
-            # mesh_3mf.SetAttachmentAsThumbnail
-            # mesh_3mf.SetPackagePart
+        # Add the mesh properties
+        mesh_3mf.SetType(Mesher._map_b3d_mesh_type_3mf[mesh_type])
+        if b3d_shape.label:
+            mesh_3mf.SetName(b3d_shape.label)
+        if part_number:
+            mesh_3mf.SetPartNumber(part_number)
+        if uuid_value:
+            mesh_3mf.SetUUID(str(uuid_value))
+        # mesh_3mf.SetAttachmentAsThumbnail
+        # mesh_3mf.SetPackagePart
 
-            # Add color
-            self._add_color(b3d_shape, mesh_3mf)
+        # Add color
+        self._add_color(b3d_shape, mesh_3mf)
 
-            # Check mesh
-            if not mesh_3mf.IsValid():
-                #raise RuntimeError("3mf mesh is invalid")
-                warnings.warn("3mf mesh is invalid")
-            if not mesh_3mf.IsManifoldAndOriented():
-                warnings.warn("3mf mesh is not manifold")
+        # Check mesh
+        if not mesh_3mf.IsValid():
+            # raise RuntimeError("3mf mesh is invalid")
+            warnings.warn("3mf mesh is invalid")
+        if not mesh_3mf.IsManifoldAndOriented():
+            warnings.warn("3mf mesh is not manifold")
 
-            # Add mesh to model
-            self.meshes.append(mesh_3mf)
-            self.model.AddBuildItem(mesh_3mf, self.wrapper.GetIdentityTransform())
+        # Add mesh to model
+        self.meshes.append(mesh_3mf)
+        self.model.AddBuildItem(mesh_3mf, self.wrapper.GetIdentityTransform())
 
-            # Not sure is this is required...
-            components = self.model.AddComponentsObject()
-            components.AddComponent(mesh_3mf, self.wrapper.GetIdentityTransform())
+        # Not sure is this is required...
+        components = self.model.AddComponentsObject()
+        components.AddComponent(mesh_3mf, self.wrapper.GetIdentityTransform())
+
+
 Mesher.add_shape = add_shape
+
 
 def export_build123d(result, path):
     export_type = path.suffix
     path = str(path)
+    if isinstance(result, ShapeList):
+        result = Compound(result)
     if export_type == ".step":
-        result.export_step(path)
+        export_step(result, path)
     elif export_type == ".brep":
-        result.export_brep(path)
+        export_brep(result, path)
     elif export_type in [".stl", ".3mf"]:
         exporter = Mesher()
         exporter.add_shape(result)
         exporter.write(path)
     else:
-        print(
-            f"Error: {result} doesn't have an exporter for {export_type}")
+        print(f"Error: {result} doesn't have an exporter for {export_type}")
+
 
 def export_cadquery(result, path):
     if path.suffix == ".brep":
@@ -136,42 +143,48 @@ def export_cadquery(result, path):
 
 
 def _do_export(module, path):
-    print(f"Importing {module}")
-    mod = import_module(module)
-    if hasattr(mod, 'result'):
+    try:
+        print(f"Importing {module}")
+        mod = import_module(module)
+    except Exception as e:
+        print(f"Failed to import {module}")
+        raise e
+
+    if hasattr(mod, "result"):
         results = {"default": mod.result}
-    elif hasattr(mod, 'results'):
+    elif hasattr(mod, "results"):
         results = mod.results
     else:
-        print(
-            f"Warning: Module {module} doesn't contain any results")
+        print(f"Warning: Module {module} doesn't contain any results")
         return
 
     for name, result in results.items():
         for t in export_types:
-            export_path = (
-                Path("export") / (
-                    path
-                    .with_stem(path.stem + f"-{name}")
-                    .with_suffix(t)
-                )
+            export_path = Path("export") / (
+                path.with_stem(path.stem + f"-{name}").with_suffix(t)
             )
             if isinstance(result, cq.Assembly):
                 result = result.toCompound()
             # Ensure export dir exists prior to writing to it
             export_dir = export_path.parent
             os.makedirs(export_dir, exist_ok=True)
-            if "cadquery" in str(type(result)):
-                print(f"Exporting CadQuery part to {export_path}")
-                export_cadquery(result, export_path)
-            else:
-                print(f"Exporting build123d part to {export_path}")
-                export_build123d(result, export_path)
+            try:
+                if "cadquery" in str(type(result)):
+                    print(f"Exporting CadQuery part to {export_path}")
+                    export_cadquery(result, export_path)
+                else:
+                    print(f"Exporting build123d part to {export_path}")
+                    export_build123d(result, export_path)
+            except Exception as e:
+                print(f"Failed to export to {export_path}")
+                raise e
 
 
 @click.command()
 @click.option("-f", "--file", "files", multiple=True)
-@click.option("-j", "--jobs", "jobs", default=0, help="Number of jobs to run in parallel")
+@click.option(
+    "-j", "--jobs", "jobs", default=0, help="Number of jobs to run in parallel"
+)
 def main(files, jobs):
     print("Cleaning export folder")
     if files:
@@ -187,11 +200,7 @@ def main(files, jobs):
         jobs = multiprocessing.cpu_count()
     export_args = []
     for path in files:
-        if (
-            path.suffix != ".py"
-            or path.stem == "export"
-            or str(path).startswith(".")
-        ):
+        if path.suffix != ".py" or path.stem == "export" or str(path).startswith("."):
             continue
         module = ".".join(path.with_suffix("").parts)
         export_args.append((module, path))
@@ -204,6 +213,6 @@ def main(files, jobs):
         for module, path in export_args:
             _do_export(module, path)
 
+
 if __name__ == "__main__":
     main()
-
--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    # Latest nixos-unstable seems to break building vtk 9.3.1
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
 
     flake-utils.url = "github:numtide/flake-utils";
 
@@ -25,114 +26,141 @@
     };
   };
 
-  outputs = {
-    self, nixpkgs, flake-utils, pyproject-nix, uv2nix, pyproject-build-systems,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      inherit (nixpkgs) lib;
-      pkgs = import nixpkgs { inherit system; };
-      python = pkgs.python312;
-
-      # cadquery-ocp needs vtk 9.3
-      vtk93Deriv = import "${pkgs.path}/pkgs/development/libraries/vtk/generic.nix" {
-        majorVersion = "9.3";
-        minorVersion = "1";
-        sourceSha256 = "sha256-g1TsCE6g0tw9I9vkJDgjxL/CcDgtDOjWWJOf1QBhyrg=";
-      };
-      vtk93 = (pkgs.callPackage vtk93Deriv {
-        enablePython = true;
-        inherit python;
-
-        # Other stuff that callPackage doesn't fill in for some reason?
-        qtdeclarative = pkgs.qt5.qtdeclarative;
-        qttools = pkgs.qt5.qttools;
-        qtx11extras = pkgs.qt5.qtx11extras;
-        qtEnv = pkgs.qt5.qtEnv;
-      }).overrideAttrs(old: {
-        # cadquery-ocp wheel looks for versioned .so file names
-        # TODO: figure out why this doesn't work
-        #cmakeFlags = (builtins.filter (f: !(builtins.match "^-DVTK_VERSIONED_INSTALL=.*" f)) old.cmakeFlags) ++ [
-        #  "-DVTK_VERSIONED_INSTALL=ON"
-        #];
-        cmakeFlags = old.cmakeFlags ++ [
-          "-DVTK_VERSIONED_INSTALL=ON"
-        ];
-      });
-
-      workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
-      overlay = workspace.mkPyprojectOverlay {
-        sourcePreference = "wheel";
-      };
-      hacks = pkgs.callPackage pyproject-nix.build.hacks {};
-      pyprojectOverrides = final: prev: {
-        cadquery-ocp = prev.cadquery-ocp.overrideAttrs (old: {
-          # Include vtk 9.3
-          buildInputs = (old.buildInputs or [ ]) ++ [ vtk93 ];
-
-          # TODO: this no longer happens once cadquery was included???
-          # HACK: OCP imports fail with
-          # `ImportError: /nix/store/5gz8cxcfjxxc5jy84cbb3pmfvhq1zcj3-cadquery-ocp-7.8.1.1.post1/lib/python3.12/site-packages/cadquery_ocp.libs/libTKIVtk-a1a167e9.so.7.8.1: undefined symbol: _ZNK9vtkObject20GetObjectDescriptionEv`
-          # if vtk isn't imported first?
-          #postInstall = ''
-          #  main_init=$out/lib/python3.12/site-packages/OCP/__init__.py
-          #  echo 'import vtk'$'\n'"$(cat $main_init)" > $main_init
-          #'';
-        });
-
-        # Casadi needs a bunch of libs like libknitro.so and I have no idea where
-        # they come from, just use a prebuilt from nixpkgs
-        casadi = hacks.nixpkgsPrebuilt {
-          from = pkgs.python3Packages.casadi;
-          prev = prev.casadi;
-        };
-
-        bd-warehouse = prev.bd-warehouse.overrideAttrs
-          ( old: { buildInputs = (old.buildInputs or [ ]) ++ [ prev.setuptools ]; } );
-      };
-      pythonSet =
-        (pkgs.callPackage pyproject-nix.build.packages {
-          inherit python;
-        }).overrideScope (
-          lib.composeManyExtensions [
-            pyproject-build-systems.overlays.default
-            overlay
-            pyprojectOverrides
-          ]
-        );
-    in {
-      devShells.default =
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      pyproject-nix,
+      uv2nix,
+      pyproject-build-systems,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
-        virtualenv = pythonSet.mkVirtualEnv "objects-devenv" workspace.deps.all;
-      in
-      pkgs.mkShell {
-        packages = [
-          virtualenv
-          pkgs.uv
-        ];
-        env = {
-          UV_NO_SYNC = "1";
-          UV_PYTHON = "${virtualenv}/bin/python";
-          UV_PYTHON_DOWNLOADS = "never";
+        inherit (nixpkgs) lib;
+        pkgs = import nixpkgs { inherit system; };
+        python = pkgs.python312;
+
+        # cadquery-ocp needs vtk 9.3
+        vtkDeriv = import "${pkgs.path}/pkgs/development/libraries/vtk/generic.nix" {
+          #version = "9.3.1";
+          majorVersion = "9.3";
+          minorVersion = "1";
+          sourceSha256 = "sha256-g1TsCE6g0tw9I9vkJDgjxL/CcDgtDOjWWJOf1QBhyrg=";
         };
-        shellHook = ''
-          # Undo dependency propagation by nixpkgs.
-          unset PYTHONPATH
+        vtk =
+          (pkgs.callPackage vtkDeriv {
+            enablePython = true;
+            inherit python;
+            #pythonSupport = true;
 
-          # Get repository root using git. This is expanded at runtime by the editable `.pth` machinery.
-          export REPO_ROOT=$(git rev-parse --show-toplevel)
+            # Other stuff that callPackage doesn't fill in for some reason?
+            qtdeclarative = pkgs.qt5.qtdeclarative;
+            qttools = pkgs.qt5.qttools;
+            qtx11extras = pkgs.qt5.qtx11extras;
+            qtEnv = pkgs.qt5.qtEnv;
+          }).overrideAttrs
+            (old: {
+              # cadquery-ocp wheel looks for versioned .so file names
+              # TODO: figure out why this doesn't work
+              #cmakeFlags = (builtins.filter (f: !(builtins.match "^-DVTK_VERSIONED_INSTALL=.*" f)) old.cmakeFlags) ++ [
+              #  "-DVTK_VERSIONED_INSTALL=ON"
+              #];
+              cmakeFlags = old.cmakeFlags ++ [
+                "-DVTK_VERSIONED_INSTALL=ON"
+              ];
+            });
+        #vtk = pkgs.vtk-full.overrideAttrs (old: {
+        #  # cadquery-ocp wheel looks for versioned .so file names
+        #  # TODO: figure out why this doesn't work
+        #  #cmakeFlags = (builtins.filter (f: !(builtins.match "^-DVTK_VERSIONED_INSTALL=.*" f)) old.cmakeFlags) ++ [
+        #  #  "-DVTK_VERSIONED_INSTALL=ON"
+        #  #];
+        #  cmakeFlags = old.cmakeFlags ++ [
+        #    "-DVTK_VERSIONED_INSTALL=ON"
+        #  ];
+        #});
 
-          # https://github.com/NixOS/nixpkgs/issues/176081#issuecomment-1145825623
-          export FONTCONFIG_FILE=${pkgs.fontconfig.out}/etc/fonts/fonts.conf
-          export FONTCONFIG_PATH=${pkgs.fontconfig.out}/etc/fonts/
-        '';
-      };
-      devShells.uv = pkgs.mkShell {
-        packages = [
-          pkgs.uv
-          python
-        ];
-      };
-    });
+        workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+        overlay = workspace.mkPyprojectOverlay {
+          sourcePreference = "wheel";
+        };
+        hacks = pkgs.callPackage pyproject-nix.build.hacks { };
+        pyprojectOverrides = final: prev: {
+          cadquery-ocp = prev.cadquery-ocp.overrideAttrs (old: {
+            buildInputs = (old.buildInputs or [ ]) ++ [ vtk ];
+
+            # TODO: this no longer happens once cadquery was included???
+            # HACK: OCP imports fail with
+            # `ImportError: /nix/store/5gz8cxcfjxxc5jy84cbb3pmfvhq1zcj3-cadquery-ocp-7.8.1.1.post1/lib/python3.12/site-packages/cadquery_ocp.libs/libTKIVtk-a1a167e9.so.7.8.1: undefined symbol: _ZNK9vtkObject20GetObjectDescriptionEv`
+            # if vtk isn't imported first?
+            postInstall = ''
+              main_init=$out/${python.sitePackages}/OCP/__init__.py
+              echo 'import vtk'$'\n'"$(cat $main_init)" > $main_init
+            '';
+          });
+
+          # Casadi needs a bunch of libs like libknitro.so and I have no idea where
+          # they come from, just use a prebuilt from nixpkgs
+          casadi = hacks.nixpkgsPrebuilt {
+            from = pkgs.python312Packages.casadi;
+            prev = prev.casadi;
+          };
+
+          bd-warehouse = prev.bd-warehouse.overrideAttrs (old: {
+            buildInputs = (old.buildInputs or [ ]) ++ [ prev.setuptools ];
+          });
+          cadquery = prev.cadquery.overrideAttrs (old: {
+            buildInputs = (old.buildInputs or [ ]) ++ [ prev.setuptools ];
+          });
+        };
+        pythonSet =
+          (pkgs.callPackage pyproject-nix.build.packages {
+            inherit python;
+          }).overrideScope
+            (
+              lib.composeManyExtensions [
+                pyproject-build-systems.overlays.default
+                overlay
+                pyprojectOverrides
+              ]
+            );
+      in
+      {
+        devShells.default =
+          let
+            virtualenv = pythonSet.mkVirtualEnv "objects-devenv" workspace.deps.all;
+          in
+          pkgs.mkShell {
+            packages = [
+              virtualenv
+              pkgs.uv
+            ];
+            env = {
+              UV_NO_SYNC = "1";
+              UV_PYTHON = "${virtualenv}/bin/python";
+              UV_PYTHON_DOWNLOADS = "never";
+            };
+            shellHook = ''
+              # Undo dependency propagation by nixpkgs.
+              unset PYTHONPATH
+
+              # Get repository root using git. This is expanded at runtime by the editable `.pth` machinery.
+              export REPO_ROOT=$(git rev-parse --show-toplevel)
+
+              # https://github.com/NixOS/nixpkgs/issues/176081#issuecomment-1145825623
+              export FONTCONFIG_FILE=${pkgs.fontconfig.out}/etc/fonts/fonts.conf
+              export FONTCONFIG_PATH=${pkgs.fontconfig.out}/etc/fonts/
+            '';
+          };
+        devShells.uv = pkgs.mkShell {
+          packages = [
+            pkgs.uv
+            python
+          ];
+        };
+      }
+    );
 }
