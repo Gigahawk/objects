6cfd8e03933a273be4fc510759a7fb150f8d677e
Jasper Chan <jasperchan515@gmail.com>
2025-12-21 13:50:58 -0800

    cpu_holder: adjust dimensions, add more printing notes

--- cpu_holder.py
+++ cpu_holder.py
@@ -4,9 +4,12 @@ Currently supports CPUs for the following sockets:
 - FCPGA946
 
 Print setting recommendations:
-- Brim ears on outside corners (Superslicer feature)
-    - Use brim blockers on the inside corners to avoid the two halves from fusing together
-
+- Brim ears on corners (Superslicer feature)
+    - Use brim blockers on the inside corners between the two
+      halves fusing them together
+- Support on build plate only
+    - Block supports inside the hinge
+- Print the tab on its side with no brim
 """
 
 from build123d import *
@@ -33,21 +36,26 @@ cpu_heatspreader_chamfer = 3
 # To compensate for printer internal radii
 cpu_corner_cutout_rad = 0.5
 
-cpu_xy_tol = 0.1
+cpu_xy_tol = 0.25
 cpu_z_tol = 0.1
 
 label_extra = 5
 finger_gap_length = 15
+finger_gap_taper_angle = 20
 marker_depth = 1
+marker_width = clamshell_wall_thickness / 1.5
 
 # Max width for PrusaSlicer to not complain about a long bridge is about 14.5
 tab_width = 12
-tab_catch_depth = 2
+tab_catch_depth = 1.6
 tab_thickness = 1
 tab_mount_hole_depth = 10
-tab_mount_hole_width_tol = 0.1
-tab_mount_hole_thickness_tol = 0.1
+tab_mount_hole_width_tol = 0.2
+tab_mount_hole_thickness_tol = 0.4
 tab_mount_hole_depth_tol = 0.05
+tab_fillet_rad = 2
+tab_end_height_tol = 1
+tab_end_chamfer = 0.8
 
 _cpu_substrate_width = cpu_substrate_width + cpu_xy_tol
 _cpu_substrate_length = cpu_substrate_height + cpu_xy_tol
@@ -66,7 +74,9 @@ tab_hole_height = (
     clamshell_thickness - (_cpu_substrate_thickness + _cpu_pin_depth)
 ) / 2
 tab_hole_height_rem = clamshell_thickness - tab_hole_height
-tab_fillet_rad = 2
+# When printing we expect the top layer to droop pushing the tab into the bottom
+# of the hole, move path down to compensate so we end up with the correct length
+tab_thickness_center_offset = tab_mount_hole_thickness_tol/2
 
 _hinge = hinge.build(
     hinge_dia=hinge_dia,
@@ -111,8 +121,8 @@ with BuildPart() as holder_parent:
         )
         with Locations(marker_loc):
             Triangle(
-                a=clamshell_wall_thickness / 2,
-                b=clamshell_wall_thickness / 2,
+                a=marker_width,
+                b=marker_width,
                 C=90,
                 align=(Align.MIN, Align.MAX),
                 rotation=180,
@@ -127,20 +137,47 @@ with BuildPart() as holder_parent:
         _corners = substrate_pocket_sketch.vertices()
         with Locations(_corners):
             Circle(cpu_corner_cutout_rad)
-        Rectangle(
-            _cpu_substrate_width + 2 * clamshell_wall_thickness, finger_gap_length
-        )
     extrude(amount=-_cpu_substrate_thickness, mode=Mode.SUBTRACT)
 
     substrate_pocket_face = (
         holder_parent.faces(Select.LAST).filter_by(Axis.Z).sort_by(Axis.Z)[0]
     )
+
+    with BuildSketch(parent_center_loc) as substrate_pocket_sketch:
+        Rectangle(
+            _cpu_substrate_width + 2 * clamshell_wall_thickness, finger_gap_length
+        )
+        Rectangle(
+            _cpu_substrate_width,
+            _cpu_substrate_length,
+            mode=Mode.SUBTRACT,
+        )
+    extrude(amount=-_cpu_substrate_thickness, mode=Mode.SUBTRACT)
+
     with BuildSketch(substrate_pocket_face) as pin_pocket_sketch:
         Rectangle(
             cpu_pin_pocket_width,
             cpu_pin_pocket_length,
         )
     extrude(amount=-_cpu_pin_depth, mode=Mode.SUBTRACT)
+
+    with BuildSketch(substrate_pocket_face) as substrate_pocket_sketch:
+        Rectangle(
+            # Just a really big value to ensure we cut all the way through even with a taper
+            (_cpu_substrate_width + 2 * clamshell_wall_thickness)*2,
+            finger_gap_length
+        )
+        Rectangle(
+            _cpu_substrate_width,
+            _cpu_substrate_length,
+            mode=Mode.SUBTRACT,
+        )
+    extrude(
+        amount=-clamshell_thickness,
+        mode=Mode.SUBTRACT,
+        taper=finger_gap_taper_angle
+    )
+
 
     with BuildSketch(parent_front_face) as tab_hole_sketch:
         with Locations(Location((0, 0, 0), (0, 0, -90))):
@@ -153,15 +190,20 @@ with BuildPart() as holder_parent:
         amount=-(tab_mount_hole_depth + tab_mount_hole_depth_tol), mode=Mode.SUBTRACT
     )
 
+    with Locations(hinge_center_loc):
+        add(_hinge["parent_cutout"], mode=Mode.SUBTRACT)
+        add(_hinge["parent"])
+
+with BuildPart() as _tab:
     with BuildLine() as tab_path:
         _base_line = FilletPolyline(
             [
-                (0, tab_mount_hole_depth, 0),
-                (0, -(tab_catch_depth + tab_thickness / 2), 0),
+                (0, tab_mount_hole_depth, -tab_thickness_center_offset),
+                (0, -(tab_catch_depth + tab_thickness / 2), -tab_thickness_center_offset),
                 (
                     0,
                     -(tab_catch_depth + tab_thickness / 2),
-                    tab_hole_height_rem + clamshell_thickness,
+                    tab_hole_height_rem + clamshell_thickness - tab_end_height_tol,
                 ),
             ],
             radius=tab_fillet_rad,
@@ -171,14 +213,16 @@ with BuildPart() as holder_parent:
 
     with BuildSketch(parent_front_face, mode=Mode.PRIVATE) as tab_profile:
         with Locations(Location((0, 0, 0), (0, 0, -90))):
-            with Locations((0, -clamshell_thickness / 2 + tab_hole_height)):
+            with Locations(
+                (0, -clamshell_thickness / 2 + tab_hole_height - tab_thickness_center_offset)
+            ):
                 Rectangle(
                     tab_width,
                     tab_thickness,
                 )
     sweep(sections=tab_profile.sketch, path=tab_path.line)
     tab_top_edge = (
-        holder_parent.edges(Select.LAST)
+        _tab.edges(Select.LAST)
         .filter_by(Axis.X)
         .sort_by(Axis.Z, reverse=True)[0:2]
         .sort_by(Axis.Y)[-1]
@@ -192,21 +236,25 @@ with BuildPart() as holder_parent:
             )
         )
         with Locations(_tab_parent_catch_sketch_local_loc):
+            _catch_height = clamshell_thickness / 2 - tab_end_height_tol
             Triangle(
-                a=clamshell_thickness / 2,
+                a=_catch_height,
                 b=tab_catch_depth,
                 C=90,
                 align=(Align.MIN, Align.MIN),
                 rotation=-90,
             )
+            with Locations((tab_catch_depth, -_catch_height)):
+                tab_chamfer_cut = Triangle(
+                    a=tab_end_chamfer,
+                    b=tab_end_chamfer,
+                    C=90,
+                    mode=Mode.SUBTRACT,
+                    rotation=0,
+                    align=(Align.MAX, Align.MIN),
+                )
     extrude(amount=tab_width / 2, both=True)
 
-    with Locations(hinge_center_loc):
-        add(_hinge["parent_cutout"], mode=Mode.SUBTRACT)
-        add(_hinge["parent"])
-
-holder_parent_main = holder_parent.part.solids().sort_by(Axis.Y)[-1]
-holder_parent_tab = holder_parent.part.solids().sort_by(Axis.Y)[0]
 
 with BuildPart() as holder_child:
     with Locations(clamshell_offset):
@@ -265,9 +313,9 @@ holder_child_rotated = holder_child.part.rotate(rotation_axis, 180)
 
 
 results = {
-    "asm_print": Compound([holder_parent_main, holder_child.part]),
+    "asm_print": Compound([holder_parent.part, holder_child.part]),
     "asm_rotated": Compound([holder_parent.part, holder_child_rotated]),
-    "tab": holder_parent_tab,
+    "tab": _tab.part,
 }
 
 
