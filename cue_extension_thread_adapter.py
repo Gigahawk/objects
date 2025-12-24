b0c187ad6bbddbf6df7f16391658fdd7b84806f7
Jasper Chan <jasperchan515@gmail.com>
2025-12-24 00:21:45 -0800

    cuetec_weight_spacer: initial geometry
    * cue_extension_thread_adapter: switch external thread to cuetec geometry, add printing notes

--- cue_extension_thread_adapter.py
+++ cue_extension_thread_adapter.py
@@ -2,11 +2,19 @@
 
 Meant to adapt the butt of this cue:
 https://www.temu.com/goods.html?_bg_fs=1&goods_id=601100587774219
+> It seems like the butt of the cue is using a standard M18x2.5 Cuetec style
+thread.
 
 To this bumper:
 https://www.temu.com/goods_snapshot.html?goods_id=601100631369613
 Note that the picture shows a coarse threaded bumper that might actually fit,
 but what I actually recieved had a much finer thread than shown in the picture
+
+Print Settings:
+- Material: not PLA, PETG preferred
+- Layer Height: 0.1mm
+- Use Arachne perimeter generator
+- Probably disable elephant foot compensation unless yours is really dialed in
 """
 
 import logging
@@ -33,18 +41,18 @@ _inner_thread = IsoThread(
     length=thread_len,
     external=False,
     end_finishes=["fade", "fade"],
+    interference=0,
 )
 
 _outer_thread = IsoThread(
-    major_diameter=17.95,
-    pitch=9.75 / 4,
+    major_diameter=18,
+    pitch=2.5,
     length=thread_len,
     external=True,
     end_finishes=["fade", "fade"],
+    interference=0,
 )
 
-# Seems like the IsoThread objects have a little extra thickness added to them
-# that causes us issues, get rid of it
 with BuildPart() as wall:
     outer_rad = _outer_thread.min_radius
     inner_rad = _inner_thread.major_diameter / 2
@@ -72,14 +80,8 @@ with BuildPart() as wall:
         align=(Align.CENTER, Align.CENTER, Align.MIN),
         mode=Mode.SUBTRACT,
     )
-with BuildPart() as inner_thread:
-    add(_inner_thread)
-    Cylinder(
-        radius=inner_rad,
-        height=thread_len,
-        align=(Align.CENTER, Align.CENTER, Align.MIN),
-        mode=Mode.INTERSECT,
-    )
+
+# Ensure outer thread is trimmed to not intersect with wall
 with BuildPart() as outer_thread:
     add(_outer_thread)
     Cylinder(
@@ -91,7 +93,7 @@ with BuildPart() as outer_thread:
 
 with BuildPart() as part:
     add(outer_thread)
-    add(inner_thread)
+    add(_inner_thread)
     add(wall)
 
 result = part.part
